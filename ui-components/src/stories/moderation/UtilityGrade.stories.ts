6816037ceefad441bdecaef549f3da3a31ebffe0
François SP <francois-sp@gmx.fr>
2025-01-08 14:58:10 +0100

    feat: add label to selector
    + Add description to stories

--- ui-components/src/stories/moderation/UtilityGrade.stories.ts
+++ ui-components/src/stories/moderation/UtilityGrade.stories.ts
@@ -8,17 +8,26 @@ const meta: any = {
   title: 'Moderation/UtilityGrade',
   component: UtilityGrade,
   args: {
+    isChatGPT: false,
+    isTeacher: false,
     // Has to have the name of the event with `on` in front and in camelCase
     onSubmitUtilityGrade: fn(),
   },
-  tags: ['autodocs']
+  tags: ['autodocs'],
+  parameters: {
+    docs: {
+      description: {
+        story: 'Change this description'
+      }
+    }
+  }
 } satisfies Meta<typeof UtilityGrade>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 
-export const Primary: Story = {
+export const DraxoByLearner: Story = {
   args: {
     possibleGrades: [
       {label: "Strongly disagree", value: "STRONGLY_DISAGREE"},
@@ -27,4 +36,50 @@ export const Primary: Story = {
       {label: "Strongly Agree", value: "STRONGLY_AGREE"}
     ],
   },
+  parameters: {
+    docs: {
+      description: {
+        story: 'A student can grade the utility of a peer grading (made with DRAXO) of their response'
+      }
+    }
+  }
 };
+
+export const ChatGPTByLearner: Story = {
+  args: {
+    possibleGrades: [
+      {label: "Strongly disagree", value: "STRONGLY_DISAGREE"},
+      {label: "Disagree", value: "DISAGREE"},
+      {label: "Agree", value: "AGREE"},
+      {label: "Strongly Agree", value: "STRONGLY_AGREE"}
+    ],
+    isChatGPT: true
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'A student can grade the utility of a peer grading (made by ChatGPT) of their response'
+      }
+    }
+  }
+}
+
+export const ChatGPTByTeacher: Story = {
+  args: {
+    possibleGrades: [
+      {label: "Strongly disagree", value: "STRONGLY_DISAGREE"},
+      {label: "Disagree", value: "DISAGREE"},
+      {label: "Agree", value: "AGREE"},
+      {label: "Strongly Agree", value: "STRONGLY_AGREE"}
+    ],
+    isChatGPT: true,
+    isTeacher: true
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'The teacher can grade the utility of a peer grading (made by ChatGPT)'
+      }
+    }
+  }
+}
--- ui-components/src/stories/moderation/UtilityGrade.vue
+++ ui-components/src/stories/moderation/UtilityGrade.vue
@@ -15,7 +15,15 @@ export interface UtilityGradeProps {
    * A Grade is an object with a label and a value.
    * The label is displayed and the value is emitted when the grade is submitted
    */
-  possibleGrades: Grade[]
+  possibleGrades: Grade[],
+  /**
+   * Wether the evaluation has been done by ChatGPT or not
+   */
+  isChatGPT: boolean
+  /**
+   * Whether the user is a teacher or not
+   */
+  isTeacher: boolean,
 }
 
 export interface UtilityGradeEvents {
@@ -53,12 +61,19 @@ const {t} = useI18n()
   <!-- Grade buttons -->
   <v-row>
     <v-col>
-      <v-btn-toggle v-model="selectedGrade" variant="text" color="#0e6eb8" rounded="0" elevation="1">
-        <v-btn v-for="(grade, index) in props.possibleGrades" :key="index" @click="setSelectedUtilityGrade(grade)"
-               :value="grade" class="text-none text-subtitle-1">
-          {{ grade.label }}
-        </v-btn>
-      </v-btn-toggle>
+      <div v-if="!props.isChatGPT && !props.isTeacher" readonly>{{ t('peer-review-label') }}</div>
+      <div v-if=" props.isChatGPT && !props.isTeacher" readonly>{{ t('chatGPT-review-student-label') }}</div>
+      <div v-if=" props.isChatGPT &&  props.isTeacher" readonly>{{ t('chatGPT-review-teacher-label') }}</div>
+
+      <div id="horizontal-grade-selector">
+        <v-btn-toggle v-model="selectedGrade" variant="text" color="#0e6eb8" rounded="0" elevation="1"
+                      style="margin-top: 10px;">
+          <v-btn v-for="(grade, index) in props.possibleGrades" :key="index" @click="setSelectedUtilityGrade(grade)"
+                 :value="grade" class="text-none text-subtitle-1">
+            {{ grade.label }}
+          </v-btn>
+        </v-btn-toggle>
+      </div>
     </v-col>
   </v-row>
   <!-- Submit button -->
@@ -78,10 +93,16 @@ const {t} = useI18n()
 <i18n>
 {
   "en": {
-    "submit": "Submit"
+    "submit": "Submit",
+    "peer-review-label": "I consider the above evaluation useful for my learning:",
+    "chatGPT-review-student-label": "I consider the ChatGPT evaluation useful for my learning:",
+    "chatGPT-review-teacher-label": "I consider the ChatGPT evaluation useful for the student's learning:"
   },
   "fr": {
-    "submit": "Soumettre"
+    "submit": "Soumettre",
+    "peer-review-label": "Je considère que l'évaluation ci-dessus est utile pour mon apprentissage :",
+    "chatGPT-review-student-label": "Je considère que l'évaluation de ChatGPT est utile pour mon apprentissage :",
+    "chatGPT-review-teacher-label": "Je considère que l'évaluation de ChatGPT est utile pour l'apprentissage de l'étudiant :"
   }
 }
 </i18n>
