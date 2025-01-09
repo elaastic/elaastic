7233f9635e7483d559f5bd7dcee9778b92977f4f
François SP <francois-sp@gmx.fr>
2025-01-09 15:38:19 +0100

    feat: add template for EvaluationReaction
    Imported UtilityGrade component, integrated i18n for report button text in English and French. Updated layout for responsiveness.

--- ui-components/src/stories/moderation/EvaluationReaction.vue
+++ ui-components/src/stories/moderation/EvaluationReaction.vue
@@ -1,13 +1,44 @@
 <script setup lang="ts">
 
+import UtilityGrade from "@/stories/moderation/UtilityGrade.vue";
+import {useI18n} from "vue-i18n";
+
+const {t} = useI18n()
+
 </script>
 
 <template>
-  <div>
-    <h1>EvaluationReaction</h1>
-  </div>
+  <v-row id="evaluation-reaction-container">
+    <v-col>
+      <UtilityGrade :is-chat-g-p-t="false" :is-teacher="false"/>
+    </v-col>
+    <v-col align-self="center">
+      <v-btn class="text-none" variant="outlined" color="#b7446f" prepend-icon="mdi-alert" id="report-btn">
+        {{ t('report') }}
+      </v-btn>
+    </v-col>
+  </v-row>
 </template>
 
 <style scoped>
+@media (max-width: 600px) {
+  #evaluation-reaction-container {
+    flex-direction: column;
+  }
+  #report-btn {
+    width: 100%;
+  }
+}
 
 </style>
+
+<i18n>
+{
+  "en": {
+    "report": "Report"
+  },
+  "fr": {
+    "report": "Signaler"
+  }
+}
+</i18n>
