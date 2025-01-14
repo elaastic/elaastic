73d9d63dd256586eb8b3c56f15be05681eb43de8
François SP <francois-sp@gmx.fr>
2025-01-14 12:38:42 +0100

    feat: export `EvaluationReaction` instead of `UtilityGrade`

--- ui-components/src/main.ts
+++ ui-components/src/main.ts
@@ -1,9 +1,9 @@
 import ConfrontingViewpoint from '@/stories/evaluation/ConfrontingViewpoint.vue'
-import UtilityGrade from '@/stories/moderation/UtilityGrade.vue';
-import { registerPlugins } from '@/plugins'
+import EvaluationReaction from '@/stories/moderation/EvaluationReaction.vue';
+import {registerPlugins} from '@/plugins'
 
 export {
   registerPlugins,
   ConfrontingViewpoint,
-  UtilityGrade,
+  EvaluationReaction,
 }
