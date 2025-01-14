134a97da54f7852ef15b057ebddf385b4bf53425
François SP <francois-sp@gmx.fr>
2025-01-14 11:10:57 +0100

    fix: add missing i18n

--- ui-components/src/stories/moderation/UtilityGrade.vue
+++ ui-components/src/stories/moderation/UtilityGrade.vue
@@ -101,6 +101,7 @@ const submitUtilityGrade = () => {
 <i18n>
 {
   "en": {
+    "submit": "Submit",
     "peer-review-label": "I consider the above evaluation useful for my learning:",
     "chatGPT-review-student-label": "I consider the ChatGPT evaluation useful for my learning:",
     "chatGPT-review-teacher-label": "I consider the ChatGPT evaluation useful for the student's learning:",
@@ -112,6 +113,7 @@ const submitUtilityGrade = () => {
     }
   },
   "fr": {
+    "submit": "Soumettre",
     "peer-review-label": "Je considère que l'évaluation ci-dessus est utile pour mon apprentissage :",
     "chatGPT-review-student-label": "Je considère que l'évaluation de ChatGPT est utile pour mon apprentissage :",
     "chatGPT-review-teacher-label": "Je considère que l'évaluation de ChatGPT est utile pour l'apprentissage de l'étudiant :",
